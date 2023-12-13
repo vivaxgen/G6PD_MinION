96dfd89bce3d3b51e92f1611a8dec21ced46078e
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-12-13 21:20:16 +0930

    add --caller argument

--- g6pd_pipeline/cmds/run_g6pd_discovery_variant_caller.py
+++ g6pd_pipeline/cmds/run_g6pd_discovery_variant_caller.py
@@ -5,12 +5,20 @@ from ngs_pipeline import cerr, cexit, check_NGSENV_BASEDIR
 from ngs_pipeline.cmds import run_targeted_variant_caller
 
 
+snakefiles = {
+    'mpileup': 'discovery_mpileup_varcall_lr.smk',
+    'freebayes': 'discovery_freebayes_varcall_lr.smk',
+    'clair3':  'discovery_clair3_varcall_lr.smk',
+}
+
+
 def init_argparser():
     p = run_targeted_variant_caller.init_argparser()
-    p.arg_dict['snakefile'].choices = ['discovery_mpileup_varcall_lr.smk',
-                                       'discovery_clair3_varcall_lr.smk',
-                                       'discovery_freebayes_varcall_lr.smk']
-    p.arg_dict['snakefile'].default = 'discovery_mpileup_varcall_lr.smk'
+    p.add_argument('--caller', choices=['mpileup', 'freebayes', 'clair3'],
+                   default=None,
+                   help='caller to be used [mpileup]')
+    p.arg_dict['snakefile'].choices = list(snakefiles.values())
+    p.arg_dict['snakefile'].default = snakefiles['mpileup']
 
     return p
 
@@ -20,6 +28,8 @@ def main(args):
     NGSENV_BASEDIR = pathlib.Path(check_NGSENV_BASEDIR())
     smk_basepath = NGSENV_BASEDIR / 'g6pd_pipeline' / 'smk'
 
+    if args.caller:
+        args.snakefile = snakefiles[args.caller]
     args.snakefile = smk_basepath / args.snakefile
     args.target = 'final'
     args.no_config_cascade = True
