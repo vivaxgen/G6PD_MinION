25642a55e24c57101a399f9c3f039ca67987a7e9
anto @ alrescha <trimarsanto@gmail.com>
2024-02-29 15:26:38 +0930

    use module path for getting to snakefile

--- g6pd_pipeline/cmds/index_reference.py
+++ g6pd_pipeline/cmds/index_reference.py
@@ -1,7 +1,9 @@
 
+# this is a wrapper to run snakefile index_reference.smk
+
 import os
 import pathlib
-from ngs_pipeline import cerr, cexit, check_NGSENV_BASEDIR
+from ngs_pipeline import cerr, cexit, get_snakefile_path, check_NGSENV_BASEDIR
 from ngs_pipeline.cmds import run_snakefile
 
 
@@ -12,11 +14,15 @@ def init_argparser():
 
 def main(args):
 
-    # NGSENV_BASEDIR is the base directory of the current pipeline (G6PD)
-    NGSENV_BASEDIR = pathlib.Path(check_NGSENV_BASEDIR())
-    smk_basepath = NGSENV_BASEDIR / 'g6pd_pipeline' / 'rules'
+    import g6pd_pipeline
 
-    args.snakefile = smk_basepath / 'index_reference.smk'
+    # NGSENV_BASEDIR is the base directory of the current pipeline (G6PD)
+    # NGSENV_BASEDIR = pathlib.Path(check_NGSENV_BASEDIR())
+    # smk_basepath = NGSENV_BASEDIR / 'g6pd_pipeline' / 'rules'
+
+    # args.snakefile = smk_basepath / 'index_reference.smk'
+    args.snakefile = get_snakefile_path('index_reference.smk',
+                                        from_module=g6pd_pipeline)
     args.no_config_cascade = True
     args.force = True
 
