50a9e2e151fd6f1d0d7886a406dfbcac1249835f
Ludwig Hoon <ldwgkshoon@gmail.com>
2024-04-18 10:18:52 +0930

    fixed minor update-script bug

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ micromamba -y install squashfuse -c conda-forge
 echo "Cloning G6PD pipeline"
 git clone https://github.com/vivaxgen/G6PD_MinION.git ${ENVS_DIR}/G6PD-pipeline
 
-ln -s ${ENVS_DIR}/G6PD-pipeline/bin/update-pipeline.sh ${BASEDIR}/bin/update-pipeline.sh
+ln -sr ${ENVS_DIR}/G6PD-pipeline/bin/update-pipeline.sh ${BASEDIR}/bin/update-pipeline.sh
 
 git clone https://github.com/nanoporetech/rerio.git ${ENVS_DIR}/rerio
 ln -sr ${ENVS_DIR}/rerio ${BASEDIR}/opt/rerio
