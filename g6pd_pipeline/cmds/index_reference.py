2b8e6206aca22d27263e6b48258a95253d440a2b
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2025-10-29 11:41:49 +0930

    fix: import namespace

--- g6pd_pipeline/cmds/index_reference.py
+++ g6pd_pipeline/cmds/index_reference.py
@@ -3,7 +3,7 @@
 
 import os
 import pathlib
-from ngs_pipeline import cerr, cexit, get_snakefile_path, check_NGSENV_BASEDIR
+from ngs_pipeline import cerr, cexit, check_NGSENV_BASEDIR
 from ngs_pipeline.cmds import run_snakefile
 
 
@@ -21,7 +21,7 @@ def main(args):
     # smk_basepath = NGSENV_BASEDIR / 'g6pd_pipeline' / 'rules'
 
     # args.snakefile = smk_basepath / 'index_reference.smk'
-    args.snakefile = get_snakefile_path('index_reference.smk',
+    args.snakefile = run_snakefile.get_snakefile_path('index_reference.smk',
                                         from_module=g6pd_pipeline)
     args.no_config_cascade = True
     args.force = True
